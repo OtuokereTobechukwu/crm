823a21ec69f7a6603cea2bac66806b949f7e01b2
Otuokere Tobechukwu <Otuokere@users.noreply.github.com>
2020-05-20 08:22:38 +0100

    made some migrations

--- accounts/views.py
+++ accounts/views.py
@@ -1,16 +1,29 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from accounts.models import *
+
 
 # Create your views here.
 
 
 def home(request):
-    return render(request, 'accounts/dashboard.html')
+    orders = Order.objects.all()
+    customers = Customer.objects.all()
+    total_customers = customers.count()
+    total_orders = orders.count()
+    delivered = orders.filter('Delivered')
+    pending = orders.filter('Pending')
+
+    context = {'orders':orders, 'customers':customers, 'total_orders':total_orders, 'delivered':delivered,
+    'pending':pending}
+    
+    return render(request, 'accounts/dashboard.html', context)
 
 
 def products(request):
-    return render(request, 'accounts/products.html')
+    products = Product.objects.all()
 
+    return render(request, 'accounts/products.html', {'products': products})
 
 def customer(request):
     return render(request, 'accounts/customer.html')
