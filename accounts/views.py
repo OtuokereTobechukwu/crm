a54a6f3a738e2630c98c047cc24eae9f807577e4
Otuokere Tobechukwu <Otuokere@users.noreply.github.com>
2020-05-20 09:14:29 +0100

    Use dynamic urls in the customer template

--- accounts/views.py
+++ accounts/views.py
@@ -11,8 +11,8 @@ def home(request):
     customers = Customer.objects.all()
     total_customers = customers.count()
     total_orders = orders.count()
-    delivered = orders.filter('Delivered')
-    pending = orders.filter('Pending')
+    delivered = orders.filter(status='Delivered').count()
+    pending = orders.filter(status='Pending').count()
 
     context = {'orders':orders, 'customers':customers, 'total_orders':total_orders, 'delivered':delivered,
     'pending':pending}
@@ -25,5 +25,10 @@ def products(request):
 
     return render(request, 'accounts/products.html', {'products': products})
 
-def customer(request):
-    return render(request, 'accounts/customer.html')
+def customer(request, pk):
+    customer = Customer.objects.get(id=pk)
+
+    orders = customer.order_set.all()
+    order_count = orders.count()
+    context = {'customer':customer, 'orders': orders, 'order_count': order_count}
+    return render(request, 'accounts/customer.html', context)
